2b7dc7937d30a8c190eba08bc7a76a0d5256d4ba
clemilson chaves <cle_milson_silva@hotmail.com>
2025-04-05 06:01:30 -0300

    refactor: restructure dashboard with new cards for month revenue and orders, enhance order details display, and update order table filter layout

--- src/pages/app/orders/order-details.tsx
+++ src/pages/app/orders/order-details.tsx
@@ -1,5 +1,6 @@
+
 import { DialogContent, DialogDescription, DialogHeader, DialogTitle } from "@/components/ui/dialog";
-import { Table, TableBody, TableCell, TableRow } from "@/components/ui/table";
+import { Table, TableBody, TableCell, TableFooter, TableHead, TableHeader, TableRow } from "@/components/ui/table";
 
 export function OrderDetails() {
   return (
@@ -11,39 +12,69 @@ export function OrderDetails() {
         <DialogDescription>Detalhes do pedido</DialogDescription>
       </DialogHeader>
 
-      <div className="space-y-6">
+      <div className="space-y-8">
             <Table>
               <TableBody>
                 <TableRow>
                   <TableCell className="text-muted-foreground">Status</TableCell>
-                  <TableCell className="flex justify-end">
-                  <div className="flex items-center gap-2">
-                    <span className="h-2 w-2 rounded-full bg-slate-400"/>
-                    <span className="font-medium text-muted-foreground">Pendente</span>
-                  </div>
-                  </TableCell>
+                  <TableCell className="flex justify-end">Pendente</TableCell>
                 </TableRow>
 
                 <TableRow>
                   <TableCell className="text-muted-foreground">Cliente</TableCell>
-                  <TableCell className="flex justify-end">
-                  <div className="flex items-center gap-2">
-                   
-                    <span className="font-medium text-muted-foreground">Clemilson Chaves</span>
-                  </div>
-                  </TableCell>
+                  <TableCell className="flex justify-end">Clemilson Chaves</TableCell>
                 </TableRow>
 
                 <TableRow>
                   <TableCell className="text-muted-foreground">Telefone</TableCell>
-                  <TableCell className="flex justify-end">
-                  <div className="flex items-center gap-2">
-                    
-                    <span className="font-medium text-muted-foreground">(11) 95283-8766</span>
-                  </div>
-                  </TableCell>
+                  <TableCell className="flex justify-end">(11) 95283-8766</TableCell>
+                </TableRow>
+
+                <TableRow>
+                  <TableCell className="text-muted-foreground">E-mail</TableCell>
+                  <TableCell className="flex justify-end"> clemilsonchaves@gmail.com</TableCell>
+                </TableRow>
+
+                <TableRow>
+                  <TableCell className="text-muted-foreground">Realizado há</TableCell>
+                  <TableCell className="flex justify-end">Há 3 minutos</TableCell>
                 </TableRow>
               </TableBody>
+            </Table>
+
+            <Table >
+              <TableHeader>
+                <TableRow >
+                  <TableHead className="text-left">Produto</TableHead>
+                  <TableHead className="text-right">Quantidade</TableHead>
+                  <TableHead className="text-right">Preço</TableHead>
+                  <TableHead className="text-right">Subtotal</TableHead>
+                </TableRow>
+              </TableHeader>
+              <TableBody>
+                <TableRow>
+                  <TableCell className="text-muted-foreground">Pizza peperone</TableCell>
+                  <TableCell className="text-muted-foreground text-right">2</TableCell>
+                  <TableCell className="text-muted-foreground text-right">R$ 60,00</TableCell>
+                  <TableCell className="text-muted-foreground text-right">R$ 120,00</TableCell>
+                </TableRow>
+              </TableBody>
+
+              <TableBody>
+                <TableRow>
+                  <TableCell className="text-muted-foreground">Pizza calabresa</TableCell>
+                  <TableCell className="text-muted-foreground text-right">1</TableCell>
+                  <TableCell className="text-muted-foreground text-right">R$ 50,00</TableCell>
+                  <TableCell className="text-muted-foreground text-right">R$ 50,00</TableCell>
+                </TableRow>
+              </TableBody>
+
+              <TableFooter>
+                <TableRow>
+                  <TableCell colSpan={3}>Total do pedido</TableCell>
+                  <TableCell className="font-medium text-right">R$ 170,00</TableCell>
+                </TableRow>
+              </TableFooter>
             </Table>
       </div>
 
--- src/pages/app/orders/order-table-filter.tsx
+++ src/pages/app/orders/order-table-filter.tsx
@@ -6,35 +6,33 @@ import { Search, X } from "lucide-react";
 
 export function OrderTableFilter(){
    return (
-    <form className="flex items-center gap-2">
-      <span className="text-sm font-semibold">Filtros</span>
-      <Input placeholder="ID do pedido"  className="h-8 w-auto"/>
-      <Input placeholder="Nome do cliente"  className="h-8 w-[320px]"/>
+    <form className="flex flex-wrap items-center gap-2"> 
+      <span className="text-sm font-semibold whitespace-nowrap">Filtros</span>
+      <Input placeholder="ID do pedido" className="h-8 w-full sm:w-auto" />
+      <Input placeholder="Nome do cliente" className="h-8 w-full sm:w-[320px]" />
 
       <Select defaultValue="all">
-        <SelectTrigger className="h-8 w-[180px]">
-          <SelectValue />
-        </SelectTrigger>
-        <SelectContent>
+      <SelectTrigger className="h-8 w-full sm:w-[180px]">
+        <SelectValue />
+      </SelectTrigger>
+      <SelectContent>
         <SelectItem value="all">Todos os status</SelectItem>
-          <SelectItem value="pending">Pendente</SelectItem>
-          <SelectItem value="canceled">Cancelado</SelectItem>
-          <SelectItem value="processing">Em preparo</SelectItem>
-          <SelectItem value="delivering">Em entrega</SelectItem>
-          <SelectItem value="delivered">Entregue</SelectItem>
-
-        </SelectContent>
-        
+        <SelectItem value="pending">Pendente</SelectItem>
+        <SelectItem value="canceled">Cancelado</SelectItem>
+        <SelectItem value="processing">Em preparo</SelectItem>
+        <SelectItem value="delivering">Em entrega</SelectItem>
+        <SelectItem value="delivered">Entregue</SelectItem>
+      </SelectContent>
       </Select>
 
-      <Button type="submit" variant="secondary" size="xs" >
-        <Search className="h-4 w-4 mr-2" />
-        Filtrar resultados
+      <Button type="submit" variant="secondary" size="xs" className="w-full sm:w-auto">
+      <Search className="h-4 w-4 mr-2" />
+      Filtrar resultados
       </Button>
 
-      <Button type="button" variant="outline" size="xs" >
-        <X className="h-4 w-4 mr-2 text-red-600" />
-        Remover Filtros
+      <Button type="button" variant="outline" size="xs" className="w-full sm:w-auto">
+      <X className="h-4 w-4 mr-2 text-red-600" />
+      Remover Filtros
       </Button>
     </form>
    )
